90aaa02fd1db0f242e139bf315ee1f467fc04a6c
Trylz <trylzengine@outlook.com>
2015-01-16 16:39:50 -0500

    depot test

---
+++ test.java
@@ -0,0 +1,25 @@
+//===============================================================================
+//
+//
+//	Author(s)				: Chayer, Philippe.
+//                            Khouma, Abdou.
+//                            Gadoury, Gabriel.
+//                            Yeo, Clotioloman Yann.
+//
+//  Université Laval ( 2015)
+//
+//  This file is part of planIFTicateur-ALPHA.
+//
+//  planIFTicateur-ALPHA is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  planIFTicateur-ALPHA is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with planIFTicateur-ALPHA.  If not, see <http://www.gnu.org/licenses/>.
+//===============================================================================
