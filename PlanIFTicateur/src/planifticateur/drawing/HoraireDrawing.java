4442e8166bebb9de90408c44adf381a72d80b598
Gabriel Gadoury <evil12twin@hotmail.com>
2015-04-02 13:22:09 -0400

    snap to grid terminé pis les troubles que javais sont réglés + mauvais placement (ex. dans l'entete) = Point(0,0) pour le moment

--- PlanIFTicateur/src/planifticateur/drawing/HoraireDrawing.java
+++ PlanIFTicateur/src/planifticateur/drawing/HoraireDrawing.java
@@ -193,18 +193,46 @@ public class HoraireDrawing {
         int pointActiviteY = 0;
         
         for (int i = 0; i <= 4; i++){
-            for (int j = 0; j <= 6; j++){
+            for (int j = 0; j <= 7; j++){
                 for (int k = 0; k <= 29; k++){
                     jumpX = caseJourWidth + k*saut;
-                    if (p.x >= jumpX && p.x <= (jumpX + saut)){
+                    if (p.x >= jumpX && p.x < (jumpX + saut)){
+                        if (p.y >= 0 && p.y < caseHeureHeight){
+                            jumpX = 0;
+                        }
+                        if (p.y >= caseJourHeight && p.y < caseJourHeight+caseHeureHeight){
+                            jumpX = 0;
+                        }
+                        if (p.y >= 2*caseJourHeight && p.y < 2*caseJourHeight+caseHeureHeight){
+                            jumpX = 0;
+                        }
+                        if (p.y >= 3*caseJourHeight && p.y < 3*caseJourHeight+caseHeureHeight){
+                            jumpX = 0;
+                        }
+                        if (p.y >= 4*caseJourHeight && p.y < 4*caseJourHeight+caseHeureHeight){
+                            jumpX = 0;
+                        }
                         pointActiviteX = jumpX;
                     }
+                    //else{
+                    //    pointActiviteX = 0;
+                    //}
                 }
                 jumpY = i*caseJourHeight + caseHeureHeight + j*caseHeureHeight;
-                if (p.y >= jumpY && p.y <= jumpY + caseHeureHeight){
+                if (p.y >= jumpY && p.y < jumpY + caseHeureHeight){
                     pointActiviteY = jumpY;
                 }
+                //else{
+                //    pointActiviteY = 0;
+                //}
             }
+        }
+        if ((p.x >= 0 && p.x < caseJourWidth)){
+            pointActiviteX = 0;
+            pointActiviteY = 0;
+        }
+        if (p.x > width*3/4){
+            
         }
         return new Point(pointActiviteX, pointActiviteY);
     }
