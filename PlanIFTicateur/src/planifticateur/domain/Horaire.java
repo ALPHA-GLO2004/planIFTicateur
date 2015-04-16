4292bdb90b28b97b6f42de4fee81baa23aa7beb9
Trylz <trylzengine@outlook.com>
2015-04-16 17:14:34 -0400

    UNe ligne en commentaire

--- PlanIFTicateur/src/planifticateur/domain/Horaire.java
+++ PlanIFTicateur/src/planifticateur/domain/Horaire.java
@@ -185,7 +185,7 @@ public class Horaire{
         for (Activite a: this.getListeActiviteComplete()){
             if (a.isSelected() == true){
                 a.setPoint(p);
-                System.out.println(a.getPoint());
+                //System.out.println(a.getPoint());
             }
         }
     }
